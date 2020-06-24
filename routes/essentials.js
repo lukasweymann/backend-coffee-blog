4b6e6fbb15bae95da7e0f64ab1dbb082c6d5ba9f
Hanna Christ <christh87@googlemail.com>
2020-06-24 13:54:47 +0200

    Implement essentials route

---
+++ routes/essentials.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+const essentials = require('./essentials.json');
+
+router.get('/:id/:info', function (req, res, next) {
+    const { id, info } = req.params;
+
+    const idAsNumber = parseInt(id, 10);
+
+    const foundEssential = essential.find((singleEssential) => {
+        return singleEssential.id === idAsNumber;
+    });
+
+    res.send(foundEssential[info]);
+});
+
+router.get('/:id', function (req, res, next) {
+    const { id } = req.params;
+
+    const idAsNumber = parseInt(id, 10);
+
+    const foundEssential = essential.find((singleEssential) => {
+        return singleEssential.id === idAsNumber;
+    });
+
+    res.send(foundEssential);
+});
+
+router.get('/', function (req, res, next) {
+    res.send(essentials);
+});
+
+module.exports = router;
