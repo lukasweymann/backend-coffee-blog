209bbe9aab0ee79b0f95206fc971ff78d0502328
Hanna Christ <christh87@googlemail.com>
2020-06-26 17:16:38 +0200

    Added route for login

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const essentialsRouter = require('./routes/essentials');
 const varietiesRouter = require('./routes/varieties');
+const loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/essentials', essentialsRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/varieties', varietiesRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
